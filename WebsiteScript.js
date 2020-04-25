a149c048d1cc8bbf54b46687fb16476cf4198065
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-04-25 15:38:43 +1000

    Add ForgotPasswordPage, WebsiteScript.js. Reformat other files. Build login form.

---
+++ WebsiteScript.js
@@ -0,0 +1,26 @@
+//JAVASCRIPT FILE FOR THE WHOLE WEBSITE
+
+//changing the top navigation bar into a toggle menu if the window size is too small
+function navbarResize() {
+	var x = document.getElementById("nav");
+	if (x.className === "navigation") {
+		x.className += " responsive";
+	} else {
+		x.className = "navigation";
+	}
+}
+
+//form validation for login
+
+
+//cancel button in login
+
+//remember me function for login
+
+
+
+//form validation for enquiry
+
+
+//form validation for sign up
+
