a4849138e3f92e002fc67b8098c8d1c45e9cad83
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-05-08 13:45:09 +1000

    Adding form items for the user settings page for doctor and patient
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

---
+++ PatientSignupHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+$dbhost = "sql12.freesqldatabase.com";
+ $dbuser = "sql12337112";
+ $dbpass = "yacY8zPDxP";
+ $db = "sql12337112";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+
+ $firstname = $_POST['fname'];
+ $lastname = $_POST['sname'];
+ //username = email
+$username = $_POST['email'];
+ $password = $_POST['password'];
+ $usertype = 'patient';
+ $DoctorId = 'none'; 
+
+ $sql = "INSERT INTO `users`(`firstname`, `lastname`, `username`, `password`, `usertype`, `doctorid`) VALUES ('$firstname', '$lastname', '$username','$password', '$usertype', '$DoctorId')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Sign up successful, now please log-in";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+//usertype
