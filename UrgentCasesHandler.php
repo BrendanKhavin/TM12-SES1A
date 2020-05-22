47d928767aeb9948efeb342ebe25605e5454d2ad
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-05-22 14:45:28 +1000

    Creating urgent cases forms and subsequent pages, styling and handler for patients to use as an alternative to the emergency option to be available in chat
    Signed-off-by: Jennifer Luu <58239721+luujennifer@users.noreply.github.com>

--- UrgentCasesHandler.php
+++ UrgentCasesHandler.php
@@ -1,1 +1,25 @@
-//urgent cases handler
+<?php
+
+$dbhost = "sql12.freesqldatabase.com";
+ $dbuser = "sql12337112";
+ $dbpass = "yacY8zPDxP";
+ $db = "sql12337112";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+
+ $firstname = $_POST['FirstName'];
+ $lastname = $_POST['LastName'];
+ $P = $_POST['PhoneNumber'];
+ $type = $_POST['emergencyType'];
+
+
+  $sql = "INSERT INTO `urgentcases`(`firstname`, `lastname`, 'phonenumber', 'emergencytype') VALUES ('$firstname', '$lastname', '$phone', '$type')";
+
+if ($conn->query($sql) === TRUE) {
+    include("PAT-UrgentCasesPage-[Success].htm");
+} else {
+    include("PAT-UrgentCasesPage-[Failed].htm");
+}
+
+$conn->close();
+//usertype
